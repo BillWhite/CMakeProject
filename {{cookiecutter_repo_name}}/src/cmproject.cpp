b8ce3eb1bb9b6c361a0531b85b71dea2b068b5da
Bill White <bill.white@griggsinst.com>
2013-10-06 10:37:36 -0400

    Deleted misnamed folder.

--- {{cookiecutter_repo_name}}/src/cmproject.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "cmproject.hpp"
-
-int
-main(int argc, char **argv)
-{
-  std::cout << CM_MESSAGE;
-}
--- {{cookiecutter_repo_name}}/test/maintest.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(MyTestCase, MyTest)
-{
-  ASSERT_TRUE(false);
-}
