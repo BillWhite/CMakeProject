b0f143e707429605a2acd83751ef16629769240c
Bill White <bill.white@griggsinst.com>
2013-10-06 10:24:11 -0400

    Initial commit of contents.

---
+++ {{cookiecutter_repo_name}}/include/cmproject.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file cmproject_H
+ *
+ * An include file.
+ */
+#ifndef cmproject_H
+#define cmproject_H
+#define CM_MESSAGE "Hi, Mom.\n"
+
+#endif /* cmproject_H */
---
+++ {{cookiecutter_repo_name}}/src/cmproject.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "cmproject.hpp"
+
+int
+main(int argc, char **argv)
+{
+  std::cout << CM_MESSAGE;
+}
