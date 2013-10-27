b9ac88867e69ffa577b5b6e70ec52f5fafd832c2
Bill White <bill.white@griggsinst.com>
2013-10-27 19:41:23 -0400

    Finished configureClang.cmake.  Need a more general way of doing compiler configuration.

--- {{cookiecutter.repo_name}}/CMakeLib/configureClang.cmake
+++ {{cookiecutter.repo_name}}/CMakeLib/configureClang.cmake
@@ -1,11 +1,20 @@
-function (configureClang clangDir)
-  set (SYSTEM_INCLUDES -nostdinc
-                       -nostdinc++
- 		       -isystem /usr/include
-		       -isystem ${CLANG_DIR}/lib/clang/3.3/include
-		       -isystem ${CLANG_DIR}/include/c++/v1)
-  set (SYSTEM_LIBS -rpath ${CLANG_DIR}/lib -lc++ -lc++abi)
-  set (CXXSTD c++11)
+function (configureClang clangDir CXXSTD CXXLIB CXXABI)
+  set (SYSTEM_INCLUDES /usr/include
+		       ${clangDir}/lib/clang/3.3/include
+		       ${clangDir}/include/c++/v1)
+  set (SYSTEM_FLAGS "-nostdinc -nostdinc++")
+  set (SYSTEM_LIBS -rpath ${clangDir}/lib -l${CXXLIB} -l${CXXABI})
   set (DEPENDENCIES -MD)
-  add_definitions(
+
+  include_directories(SYSTEM ${SYSTEM_INCLUDES})
 endfunction(configureClang)
+
+function (configureTargetForCompilation target sources)
+  message("Configuring ${target}")
+  set_target_properties(${target}
+                        PROPERTIES
+                        LINK_FLAGS "${SYSTEM_LIBS}")
+  set_source_files_properties("${sources}"
+			      PROPERTIES
+			      COMPILE_FLAGS "${SYSTEM_FLAGS}")
+endfunction(configureTargetForCompilation)
