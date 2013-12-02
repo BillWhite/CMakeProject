611a1d720c7ca7536892ba2b893dfcb66c244664
Bill White <bill.white@griggsinst.com>
2013-12-01 20:19:04 -0500

    Deleted build.sh after all.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-rm -rf default_project
-cookiecutter .
-cd default_project
-/bin/sh default_project/pbin/configure-default_project --host-configuration default_project
-cd Debug
-ninja
-ninja ctestverbose
